d6d561c5bc17f6dc8b5a2fb7ec6429857517863a
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-08-19 22:48:14 +0300

    New learnsy

--- Learnsy/ContentView.swift
+++ Learnsy/ContentView.swift
@@ -6,32 +6,14 @@
 //
 
 import SwiftUI
-import FirebaseAuth
-import FirebaseFirestore
 
 struct ContentView: View {
-    @EnvironmentObject private var authViewModel: AuthViewModel
-    @EnvironmentObject private var chatViewModel: ChatViewModel
-
     var body: some View {
         Group {
-            switch authViewModel.userState {
-            case .loggedOut:
-                AuthView()
-            case .loading, .loggedIn:
-                TabsView()
-            case .profileEditRequired:
-                EditProfileView()
-            case .blocked(let reason):
-                BlockedView(text: reason)
-            }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .environmentObject(AuthViewModel.mock)
-        .environmentObject(ChatViewModel.mock)
-        .environmentObject(CallManager.mock)
 }
